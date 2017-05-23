dff6762d9cb81804d4f3a09d9cf11638e5e0685f
taha uygun <tahauygun@gmail.com>
2017-05-23 18:11:58 -0400

    twitter api callbacks

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'G1q65Lu2FoUm6KFVEGcxSMGpv'
+    ,consumer_secret:     'Mhv24QNKfzOFOB05E84TVWGiPoJdYV6rxe4j5ImyOrES33JdWt'
+    ,access_token:        '2887705560-UeVrHJMoCTnnwyZYgT9dNmmI1zl08G474Cm2mql'
+    ,access_token_secret: 'kMn692uqgUukiNLhbd5HMnLl5fUAqdolC4XSrIs3i5NKS'
+}
